4103a7b516d8e9d2daeea0a634b05de9cb627c75
Caline <calineyammine123@gmail.com>
2022-02-05 15:25:08 +0200

    Added dog image from api

--- javascript_solution.js
+++ javascript_solution.js
@@ -1,1 +1,7 @@
-console.log("test")
+async function getDogPic(){
+    const response= await fetch("https://dog.ceo/api/breeds/image/random");
+    const img=await response.json();
+    document.getElementById("dogImage").src=img.message;
+}
+
+getDogPic();
