a13e316e7e3e9ec827e80f73dafef7de790c4628
Caline <calineyammine123@gmail.com>
2022-02-05 21:19:57 +0200

    Added api to get country codes

--- javascript_solution.js
+++ javascript_solution.js
@@ -8,16 +8,32 @@ submit_btn.addEventListener("click",()=>{
         result.textContent="Please enter a name first.";
     }
     else{
-        getGender(name);
+        getGender(name,getAge);
     }
 });
-async function getGender(name){
+
+async function getGender(name, nextFunction){
     const response=await fetch("https://api.genderize.io?name="+name);
     const gender=await response.json();
-    result.textContent=gender.gender;
+    result.innerHTML="Gender: "+ gender.gender;
+    nextFunction(name,getNationality);
 }
-
-
+async function getAge(name,getNationality){
+    const response=await fetch("https://api.agify.io/?name="+name);
+    const age=await response.json();
+    result.innerHTML+="<br>Age: "+ age.age;
+    getNationality(name);
+}
+async function getNationality(name){
+    const response=await fetch("https://api.nationalize.io/?name="+name);
+    const country=await response.json();
+    const country_codes=await country.country;
+    result.innerHTML+="<br>Country: ";
+    country_codes.map(function (code){
+        result.innerHTML+=code.country_id+" ";
+    });
+}
+    
 async function getDogPic(){
     const response= await fetch("https://dog.ceo/api/breeds/image/random");
     const img=await response.json();
