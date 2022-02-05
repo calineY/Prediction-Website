7014107383c7b1eaf0c57141325dfaaacbf6b067
Caline <calineyammine123@gmail.com>
2022-02-05 22:04:07 +0200

    Made all data display at once

--- javascript_solution.js
+++ javascript_solution.js
@@ -1,43 +1,50 @@
 let submit_btn=document.getElementById("submit");
 let input=document.getElementById("name");
 let result=document.getElementById("response");
+let gender;
+let age;
+let country_codes;
 
+getDogPic();
 submit_btn.addEventListener("click",()=>{
     let name=input.value;
     if(!name){
         result.textContent="Please enter a name first.";
     }
     else{
+        result.innerHTML="Processing..."
         getGender(name,getAge);
     }
 });
-
-async function getGender(name, nextFunction){
-    const response=await fetch("https://api.genderize.io?name="+name);
-    const gender=await response.json();
+function print(){
     result.innerHTML="Gender: "+ gender.gender;
-    nextFunction(name,getNationality);
-}
-async function getAge(name,getNationality){
-    const response=await fetch("https://api.agify.io/?name="+name);
-    const age=await response.json();
     result.innerHTML+="<br>Age: "+ age.age;
-    getNationality(name);
-}
-async function getNationality(name){
-    const response=await fetch("https://api.nationalize.io/?name="+name);
-    const country=await response.json();
-    const country_codes=await country.country;
     result.innerHTML+="<br>Country: ";
     country_codes.map(function (code){
         result.innerHTML+=code.country_id+" ";
     });
 }
-    
+async function getGender(name, getAge){
+    const response=await fetch("https://api.genderize.io?name="+name);
+    gender=await response.json();
+    //result.innerHTML="Gender: "+ gender.gender;
+    getAge(name,getNationality);
+}
+async function getAge(name,getNationality){
+    const response=await fetch("https://api.agify.io/?name="+name);
+    age=await response.json();
+    //result.innerHTML+="<br>Age: "+ age.age;
+    getNationality(name);
+}
+async function getNationality(name){
+    const response=await fetch("https://api.nationalize.io/?name="+name);
+    const country=await response.json();
+    country_codes=country.country;
+    print();
+}
 async function getDogPic(){
     const response= await fetch("https://dog.ceo/api/breeds/image/random");
     const img=await response.json();
     document.getElementById("dogImage").src=img.message;
 }
 
-getDogPic();
